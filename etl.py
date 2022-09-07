741b5d7d7723c0f7f4a5522732203d556227c036
Ashmaawyy <al_ashmawy@outlook.com>
2022-09-07 14:25:03 +0200

    Added the work done to etl.py : )

--- etl.py
+++ etl.py
@@ -7,37 +7,53 @@ from sql_queries import *
 
 def process_song_file(cur, filepath):
     # open song file
-    df = 
+    songs_df = pd.read_json(filepath, orient = 'index')
+    songs_df = songs_df.transpose()
 
     # insert song record
-    song_data = 
+    song_data = songs_df.iloc[:, [1, 6, 7, 8, 9]].values.tolist()
+    song_data = song_data[0]
+
     cur.execute(song_table_insert, song_data)
     
     # insert artist record
-    artist_data = 
+    artist_data = songs_df.iloc[:, [1, 2, 3, 4, 5]].values.tolist()
+    artist_data = artist_data[0]
+    
     cur.execute(artist_table_insert, artist_data)
 
 
 def process_log_file(cur, filepath):
     # open log file
-    df = 
+    logs_df = pd.read_json(filepath, lines = True) # lines = True for trailing data Error
 
     # filter by NextSong action
-    df = 
+    next_song_filter_df = logs_df[logs_df['page'] == 'NextSong']
 
     # convert timestamp column to datetime
-    t = 
+    next_song_filter_df['ts'] = pd.to_datetime(next_song_filter_df['ts'])
     
     # insert time data records
-    time_data = 
-    column_labels = 
-    time_df = 
+    time_stamp = next_song_filter_df['ts']
+    hour = next_song_filter_df['ts'].dt.hour
+    day = next_song_filter_df['ts'].dt.day
+    week_of_year = next_song_filter_df['ts'].dt.weekofyear
+    month = next_song_filter_df['ts'].dt.month
+    year = next_song_filter_df['ts'].dt.year
+    weekday = next_song_filter_df['ts'].dt.weekday
+
+    time_data = [time_stamp, hour, day, week_of_year, month, year, weekday]
+    column_labels = ('timestamp', 'hour', 'day', 'week of year', 'month', 'year', 'weekday')
+
+    time_dict = {column_labels[i]: time_data[i] for i in range(len(column_labels))}
+    time_df = pd.DataFrame.from_dict(time_dict)
 
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
     # load user table
-    user_df = 
+    user_df = logs_df.iloc[:, [17, 2, 5, 3, 7]]
+    user_df = user_df.drop_duplicates()
 
     # insert user records
     for i, row in user_df.iterrows():
@@ -75,16 +91,16 @@ def process_data(cur, conn, filepath, func):
     # iterate over files and process
     for i, datafile in enumerate(all_files, 1):
         func(cur, datafile)
-        conn.commit()
         print('{}/{} files processed.'.format(i, num_files))
 
 
 def main():
     conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
     cur = conn.cursor()
+    conn.set_session(autocommit = True)
 
-    process_data(cur, conn, filepath='data/song_data', func=process_song_file)
-    process_data(cur, conn, filepath='data/log_data', func=process_log_file)
+    process_data(cur, conn, filepath = 'data/song_data', func = process_song_file)
+    process_data(cur, conn, filepath = 'data/log_data', func = process_log_file)
 
     conn.close()
 
